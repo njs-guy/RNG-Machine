2ba5679a9231b668c0226b4b47b7e80c6f1be3dd
Nick Shockey <shockeynj@gmail.com>
2021-09-04 17:02:30 -0400

    Added electron scripts

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width : 1100,
+        height: 600,
+        icon: path.join(__dirname, "./build/r_icon.png"),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(
+        url.format({
+        pathname: path.join(__dirname, "./build/index.html"),
+        protocol: "file",
+        slashes: true
+        })
+       );
+
+    //mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.on("ready", loadMainWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
